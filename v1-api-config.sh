fbdf549f5e95fdce41bf30d99f6470d710a43291
Micah Zoltu <micah@zoltu.net>
2021-04-05 17:43:40 +0800

    Create v1-api-config.sh

---
+++ v1-api-config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+influx v1 dbrp create \
+	--bucket-id ${DOCKER_INFLUXDB_INIT_BUCKET_ID} \
+	--db ${DOCKER_INFLUXDB_INIT_BUCKET} \
+	--rp ${DOCKER_INFLUXDB_INIT_BUCKET}-retention-policy \
+	--default \
+	--org ${DOCKER_INFLUXDB_INIT_ORG}
+
+influx v1 auth create \
+	--username ${DOCKER_INFLUXDB_INIT_USERNAME} \
+	--password ${DOCKER_INFLUXDB_INIT_PASSWORD} \
+	--write-bucket ${DOCKER_INFLUXDB_INIT_BUCKET_ID} \
+	--org ${DOCKER_INFLUXDB_INIT_ORG}
