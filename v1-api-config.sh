0051cbb5d0e1a88b5865f7e81d56356e9c3843a2
Micah Zoltu <micah@zoltu.net>
2021-04-08 11:53:32 +0800

    Update v1-api-config.sh

--- v1-api-config.sh
+++ v1-api-config.sh
@@ -2,14 +2,14 @@
 set -e
 
 influx v1 dbrp create \
-        --bucket-id ${DOCKER_INFLUXDB_INIT_BUCKET_ID} \
-        --db ${DOCKER_INFLUXDB_INIT_BUCKET} \
-        --rp ${DOCKER_INFLUXDB_INIT_BUCKET}-retention-policy \
-        --default \
-        --org ${DOCKER_INFLUXDB_INIT_ORG}
+--bucket-id ${DOCKER_INFLUXDB_INIT_BUCKET_ID} \
+	--db ${DOCKER_INFLUXDB_INIT_BUCKET} \
+	--rp ${DOCKER_INFLUXDB_INIT_BUCKET}-retention-policy \
+	--default \
+	--org ${DOCKER_INFLUXDB_INIT_ORG}
 
 influx v1 auth create \
-        --username ${DOCKER_INFLUXDB_INIT_USERNAME} \
-        --password ${DOCKER_INFLUXDB_INIT_PASSWORD} \
-        --write-bucket ${DOCKER_INFLUXDB_INIT_BUCKET_ID} \
-        --org ${DOCKER_INFLUXDB_INIT_ORG}
+	--username ${DOCKER_INFLUXDB_INIT_USERNAME} \
+	--password ${DOCKER_INFLUXDB_INIT_PASSWORD} \
+	--write-bucket ${DOCKER_INFLUXDB_INIT_BUCKET_ID} \
+	--org ${DOCKER_INFLUXDB_INIT_ORG}
